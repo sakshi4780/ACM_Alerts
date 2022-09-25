57a6f7eec4b91152ca49fe9b494f6d2c3ea4d19b
apoorvaron <apoorv.aron02@gmail.com>
2022-09-25 11:40:55 +0530

    blogs done

--- eventPage.js
+++ eventPage.js
@@ -4,13 +4,20 @@ let button1Text;
 let button2Text;
 let button1Link;
 let button2Link;
-
+let blogId = 0;
+let previous_blogs;
+let current_blogs;
 // current year , URL , headers for fetching the data
 let year = new Date().getFullYear();
 let yearLink = `https://usict.acm.org/eventYear.php?year=${year}`;
+let blogLiveLink = `https://usict.acm.org/singleBlog.php?Id=${blogId}`;
 
 const url = new URL(
   `http://localhost/acm-new/admin/blogadmin/api.php/?q=readAllEvent&year=${year}`
+);
+
+const blogAPIURL = new URL(
+  `https://usict.acm.org/admin/blogAdmin/api.php/?q=readAll`
 );
 
 let headers = {
@@ -34,12 +41,8 @@ chrome.runtime.setUninstallURL(uninstallURL, () => {
 });
 
 let installReason = (details) => {
-  console.log(details.reason);
+  // console.log(details.reason);
   if (details.reason === "install") {
-    // chrome.tabs.create({
-    //   url: installURL,
-    // });
-
     fetch(url, {
       method: "GET",
       headers: headers,
@@ -49,6 +52,18 @@ let installReason = (details) => {
         // use of local storage
         chrome.storage.local.set({ numberOfEvents: data[0].length });
       });
+
+    // fetch blog data
+    fetch(blogAPIURL, {
+      method: "GET",
+      headers: headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // use of local storage
+        chrome.storage.local.set({ numberOfBlogs: data[0].length });
+      });
+
     chrome.notifications.create({
       title: "ACM Alerts",
       message: `Extension is Added Successfully`,
@@ -91,11 +106,33 @@ function notification(eventName) {
     priority: 2,
   });
 }
+function notificationBlogs(blogTitle, blogAuthor, Sno) {
+  chrome.notifications.create({
+    title: "ACM Alerts",
+    message: `Blog : ${blogTitle} \nBy - ${blogAuthor}\n\nTap to view`,
+    iconUrl: "./assets/logo.png",
+    type: "basic",
+    priority: 2,
+  });
+}
 chrome.notifications.onClicked.addListener(function (notifId) {
   // console.log(yearLink);
+  console.log(blogId);
   // window.open(link, "_blank");
+  // if (blogId) {
+  //   chrome.tabs.create({
+  //     url: blogLiveLink,
+  //   });
+  // }
+  console.log(button1Text);
+  console.log(button2Text);
 
-  if (button2Text !== "" && button1Text !== "") {
+  if (
+    button2Text !== "" &&
+    button1Text !== "" &&
+    button2Text !== undefined &&
+    button1Text !== undefined
+  ) {
     if (button1Text.includes("Register")) {
       chrome.tabs.create({
         url: button1Link,
@@ -113,10 +150,20 @@ chrome.notifications.onClicked.addListener(function (notifId) {
     chrome.tabs.create({
       url: button2Link,
     });
-  } else {
+  } else if (button2Text == "" && button1Text == "") {
     chrome.tabs.create({
       url: yearLink,
     });
+  } else {
+    // console.log(12);
+    if (blogId) {
+      // console.log(21);
+      // console.log(blogId);
+      console.log(blogLiveLink);
+      chrome.tabs.create({
+        url: `https://usict.acm.org/singleBlog.php?Id=${blogId}`,
+      });
+    }
   }
 });
 
@@ -147,6 +194,32 @@ function countNumberOfEvents() {
         button2Link = data[0][data[0].length - 1].button2Link;
 
         notification(eventName);
+      }
+    });
+
+  // fethcing blogs
+  chrome.storage.local.get(["numberOfBlogs"], (numberOfBlogs) => {
+    previous_blogs = numberOfBlogs.numberOfBlogs;
+  });
+  fetch(blogAPIURL, {
+    method: "GET",
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // use of local storage
+      chrome.storage.local.set({ numberOfBlogs: data[0].length });
+      current_blogs = data[0].length;
+
+      // console.log(previous_events + " " + current_events);
+
+      if (current_blogs > previous_blogs - 1) {
+        // console.log(data[0][data[0].length - 1]);
+        blogTitle = data[0][data[0].length - 1].Title;
+        blogAuthor = data[0][data[0].length - 1].Author;
+        blogId = data[0][data[0].length - 1].Sno;
+        // console.log(blogTitle);
+        notificationBlogs(blogTitle, blogAuthor, blogId);
       }
     });
 }
